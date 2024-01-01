9e2acb9b92eab5d59f06e5f214d5b8e60a2a39e3
hackerking930 <84374203+hackerking930@users.noreply.github.com>
2024-01-01 16:42:00 +0530

    Add files via upload

---
+++ checkout/response.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cashfree - PG Response Details</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<h1 align="center">PG Response</h1>	
+
+	<?php  
+		 $secretkey = "2a126ddc4f49f644dce780c1a68612c73fd6ec43";
+		 $orderId = $_POST["orderId"];
+		 $orderAmount = $_POST["orderAmount"];
+		 $referenceId = $_POST["referenceId"];
+		 $txStatus = $_POST["txStatus"];
+		 $paymentMode = $_POST["paymentMode"];
+		 $txMsg = $_POST["txMsg"];
+		 $txTime = $_POST["txTime"];
+		 $signature = $_POST["signature"];
+		 $data = $orderId.$orderAmount.$referenceId.$txStatus.$paymentMode.$txMsg.$txTime;
+		 $hash_hmac = hash_hmac('sha256', $data, $secretkey, true) ;
+		 $computedSignature = base64_encode($hash_hmac);
+		 if($txStatus == "SUCCESS") {
+			 header("location:../thanks.php");
+		 }
+		 else {
+			header("location:../407.php");
+		 }
+		 if ($signature == $computedSignature) {
+	 ?>
+	
+	</div>	
+	</div>
+	
+	<?php	
+	 	}
+	 ?>
+
+</body>
+</html>
+
+
+
